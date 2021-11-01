b3a493afa3e4c45c64659bcbef6054464ebc1cc2
Kilian Freitag <kilian.freitag@diefreitags.de>
2021-11-01 17:53:50 +0100

    continue q_learning

--- ann_qlearning/tictactoe.py
+++ ann_qlearning/tictactoe.py
@@ -19,7 +19,7 @@ win_reward = 1
 lose_reward = -1
 tie_reward = 0
 
-n_games = 3#30000
+n_games = 50#30000
 
 def update_state(state, _q_table, epsilon, player_id):
 
@@ -27,10 +27,15 @@ def update_state(state, _q_table, epsilon, player_id):
     
     r = np.random.rand()
     
-    possible_actions = np.array([(idx, value) for (idx, value) in enumerate(q_values.flatten()) if not np.isnan(value) ])
-    if len(possible_actions) < 1:
-        print('ERROR: all fields already filled, game ended')
-        return
+    if np.all(np.isnan(q_values)) and not np.count_nonzero(state==0) == 0:
+        print('ERROR: q values all zero but still a free spot')
+        print(_q_table)
+
+    #possible_actions = np.array([(idx, value) for (idx, value) in enumerate(q_values.flatten()) if not np.isnan(value) ])
+    if np.all(np.isnan(q_values)): #len(possible_actions) < 1:
+        #print('ERROR: all fields already filled, game ended')
+        return state
+
     if r < (1-epsilon):
         # find max of q table
         action_idx = np.unravel_index(np.argmax(q_values, axis=None), q_values.shape)
@@ -41,10 +46,12 @@ def update_state(state, _q_table, epsilon, player_id):
 
     new_state = copy.copy(state)
     new_state[action_idx[0], action_idx[1]] = player_id
+
     return new_state
 
 def game_status(state):
     # check if tie
+    print(np.count_nonzero(state==0))
     if np.count_nonzero(state==0) == 0:
         return KEY_TIE
     # check if someone has won
@@ -53,6 +60,7 @@ def game_status(state):
     possible_winning_comb[3:6,:] = state.T
     possible_winning_comb[6,:] = state.diagonal()
     possible_winning_comb[7,:] = np.fliplr(state).diagonal()
+    #print('check')
     for row in possible_winning_comb:
         if np.all(row == KEY_P1):
             return KEY_WON_P1
@@ -81,7 +89,6 @@ def get_q_values(_q_table, state):
 
 def update_q_table(_q_table, state, reward, player_id):
 
-    #q_values = np.zeros([3,3])
     current_q_values = get_q_values(_q_table, state)
     it = np.nditer(state, flags=['multi_index'])
     for x in it:
@@ -92,11 +99,16 @@ def update_q_table(_q_table, state, reward, player_id):
 
             next_q_values = get_q_values(_q_table, next_state)
 
-            current_q_values[it.multi_index] += alpha*(reward + gamma * np.nanmax(next_q_values) - current_q_values[it.multi_index])
+            max_q_values = np.nanmax(next_q_values)
+            if max_q_values == np.nan:
+                max_q_values = 0
+
+            current_q_values[it.multi_index] += alpha*(reward + gamma * max_q_values - current_q_values[it.multi_index])
         else:
             current_q_values[it.multi_index] = np.nan
 
-    state_idx = [i for (i, val) in enumerate(_q_table['states']) if np.array_equal(val,state)]
+    #state_idx = [i for (i, val) in enumerate(_q_table['states']) if np.array_equal(val,state)]
+    state_idx = [i for (i, val) in enumerate(_q_table['states']) if (val==state).all()]
     if len(state_idx) == 1:
         _q_table['q_values'][state_idx[0]] = current_q_values
     elif len(state_idx) == 0:
@@ -116,7 +128,7 @@ if __name__ == '__main__':
 
     for i in range(n_games):
 
-        print('new game')
+        print('game ', i)
         # init board
         result = None
         reward = 0
@@ -157,16 +169,12 @@ if __name__ == '__main__':
                 # update Q1
                 q_table1 = update_q_table(q_table1, board, reward, KEY_P1)
 
-            #print('Board: ', board)
-
-        #print('Table 1 ', q_table1)
-        #print('Table 2', q_table2)
-
         print('Board: ', board)
         print('Result: ', result)
 
-        # update q1 and q2 with corresponding reward
+        # update q1 and q2 with corresponding reward, reward only on last position of players
         if _status == KEY_WON_P1:
+            
             q_table1 = update_q_table(q_table1, board, win_reward, KEY_P1)
             q_table2 = update_q_table(q_table2, board, lose_reward, KEY_P2)
         elif _status == KEY_WON_P2:
@@ -176,7 +184,8 @@ if __name__ == '__main__':
             q_table1 = update_q_table(q_table1, board, tie_reward, KEY_P1)
             q_table2 = update_q_table(q_table2, board, tie_reward, KEY_P2)  
         else:
-            print('ERROR: unexpeted result')
+            print('ERROR: unexpected result')
 
 
-
+print('qt 1: ', q_table1)
+print('qt 2: ', q_table2)
